d956040f6f9274eb422ae245ab5fe32476d543ef
Madina Mel <wolfi251199@gmail.com>
2023-02-14 00:52:22 +0100

    Added project2

--- Tasks_React/project2/src/App.js
+++ Tasks_React/project2/src/App.js
@@ -2,49 +2,26 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAllClicks] = useState([]);
-  // const [clicks, setClicks] = useState({
-  //   left: 0,
-  //   right: 0,
-  // });
 
-  // const leftClick = () => {
-  //   const newClicks = {
-  //     ...clicks,
-  //     left: clicks.left + 1,
-  //   };
-  //   setClicks(newClicks);
-  // };
-
-  // const rightClick = () => {
-  //   const newClicks = {
-  //     ...clicks,
-  //     right: clicks.right + 1,
-  //   };
-  //   setClicks(newClicks);
-  // };
-
-  const leftClick = () => {
-    setLeft(left + 1);
-    // setAllClicks(allClicks.concat('L'));
-    setAllClicks([...allClicks, 'L']);
-  }
-
-  const rightClick = () => {
-    setRight(right + 1);
-    // setAllClicks(allClicks.concat('R'));
-    setAllClicks([...allClicks, 'R']);
-  }
-
+  const [notes, setNotes] = useState([
+  {
+    id: 1,
+    text: 'React Router Dom'
+  },
+  {
+    id: 2,
+    text: 'Axios vs Fetch'
+  },
+]);
+  
   return (
     <div className="App">
-      {left}
-      <button onClick={leftClick}>left</button>
-      <button onClick={rightClick}>right</button>
-      {right}
-      <p>История кликов: {allClicks.join(' ')}</p>
+      <h1>Темы занятий</h1>
+      <ul>
+        {
+          notes.map(note => <li key={note.id}>{note.text}</li>)
+        }
+      </ul>
     </div>
   );
 }
